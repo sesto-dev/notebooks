f83480c2ec2dcc965f4183ac10f94c1b5e95622f
Amirhossein Mohammadi <accretence@outlook.com>
2024-09-27 08:51:34 +0330

    fractal

---
+++ sesto/highlowfinder.py
@@ -0,0 +1,184 @@
+import pandas as pd
+
+def high_low_finder(data):
+    data['time'] = pd.to_datetime(data['time'], unit='s')
+
+    data = data.dropna()
+    data.reset_index(drop=True, inplace=True)
+
+    current_leg = False
+    determination_candle = False
+    hl_situation = False
+
+    before_ignore_idx = 0
+    ignored = False
+    ignore_case = False
+
+    candle_side_list = []
+    candle_side_list.append(0)
+
+    for idx in range(1, len(data)):
+        if not ignored:
+            #getting features
+            high_base = data.iloc[idx-1].high
+            low_base = data.iloc[idx-1].low
+            high1 = data.iloc[idx].high
+            low1 = data.iloc[idx].low
+            
+            candle_side = 0
+            #check for cases
+            if high_base >= high1 and low_base <= low1:
+                #case 3
+                #Inside
+                if not current_leg:
+                    before_ignore_idx = idx-1
+                    determination_candle = idx-1
+                    ignored = True
+                    
+                elif current_leg:
+                    before_ignore_idx = idx-1
+                    ignored = True
+                    determination_candle = idx-1
+                ignore_case = 3
+                candle_side = 0
+            elif high_base < high1 and low_base > low1:
+                #case 4
+                #Engulf
+                ignore_case = 4
+                if current_leg:
+                    # if ignored:
+                    #     high_base = data.iloc[before_ignore_idx].high
+                    #     low_base = data.iloc[before_ignore_idx].low
+
+                    determination_candle = idx
+                candle_side = 0
+            
+            elif high_base < high1:
+                #case 1
+                if not current_leg:
+                    current_leg = 'up'
+                    candle_side = 'up'
+                elif current_leg == 'up':
+                        if ignore_case == '4':
+                            ignore_case = False
+                            candle_side = 0
+                        else:
+                            candle_side = 0
+                elif current_leg == 'down':
+                    if ignore_case == '4':
+                        candle_side = 0
+                        candle_side_list[determination_candle] = 'down'
+                        ignore_case = False
+                        current_leg = 'up'
+                    else:
+                        candle_side = 0
+                        candle_side_list[idx-1] = 'down'
+                        current_leg = 'up'
+            elif low_base > low1:
+                #case 2
+                if not current_leg:
+                    current_leg = 'down'
+                    candle_side = 'down'
+                elif current_leg == 'up':
+                        if ignore_case == '4':
+                            candle_side = 0
+                            candle_side_list[determination_candle] = 'up'
+                            current_leg = 'down'
+                            ignore_case = False
+                        else:
+                            candle_side_list[idx-1] = 'up'
+                            current_leg = 'down'
+                            candle_side = 0
+                elif current_leg == 'down':
+                    if ignore_case == '4':
+                        candle_side = 0
+                        ignore_case = False
+                    else:
+                        candle_side = 0
+                ignored = False
+            candle_side_list.append(candle_side)
+            continue    
+                
+        elif ignored:
+            high_base = data.iloc[before_ignore_idx].high
+            low_base = data.iloc[before_ignore_idx].low
+            high1 = data.iloc[idx].high
+            low1 = data.iloc[idx].low
+            
+            candle_side = 0
+            #check for cases
+            if high_base >= high1 and low_base <= low1:
+                #case 3
+                #Inside
+                # if not current_leg:
+                #     before_ignore_idx = idx
+                #     determination_candle = idx
+                #     ignored = True
+                    
+                # if current_leg:
+                #     before_ignore_idx = idx
+                #     ignored = True
+                #     determination_candle = idx
+                ignore_case = 3
+                candle_side = 0
+            elif high_base < high1 and low_base > low1:
+                #case 4
+                #Engulf
+                ignore_case = 4
+                if not current_leg:
+                    before_ignore_idx = idx
+                    determination_candle = idx
+                    ignored = True
+                if current_leg:
+                    before_ignore_idx = idx
+                    ignored = True
+                    determination_candle = idx
+                candle_side = 0
+            
+            elif high_base < high1:
+                #case 1
+                if not current_leg:
+                    current_leg = 'up'
+                    candle_side = 'up'
+                elif current_leg == 'up':
+                        if ignore_case == '4':
+                            candle_side = 0
+                            current_leg = 'up'
+                        else:
+                            candle_side = 0
+                            current_leg = 'up'
+                elif current_leg == 'down':
+                    if ignore_case == '4':
+                        candle_side = 0
+                        candle_side_list[determination_candle] = 'down'
+                        current_leg = 'up'
+                    else:
+                        candle_side_list[determination_candle] = 'down'
+                        candle_side = 0
+                        current_leg = 'up'
+                ignored = False
+                ignore_case == False
+            elif low_base > low1:
+                #case 2
+                if not current_leg:
+                    current_leg = 'down'
+                    candle_side = 'down'
+                elif current_leg == 'up':
+                        if ignore_case == '4':
+                            candle_side = 0
+                            current_leg = 'down'
+                            candle_side_list[determination_candle] = 'up'
+                        else:
+                            candle_side_list[determination_candle] = 'up'
+                            candle_side = 0
+                            current_leg = 'down'
+                elif current_leg == 'down':
+                    if ignore_case == '4':
+                        candle_side = 0
+                    else:
+                        candle_side = 0
+                ignored = False
+                ignore_case = False
+            candle_side_list.append(candle_side)
+    
+    return candle_side_list
